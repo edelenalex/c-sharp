6afd30447efc45681407afda3de5503de5a6740f
Alex Edelen <55199498+edelenalex@users.noreply.github.com>
2020-12-01 21:28:13 -0500

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace Program_2
+﻿namespace Program1
 {
-    partial class Program2
+    partial class Program1
     {
         /// <summary>
         /// Required designer variable.
@@ -28,89 +28,171 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lastNameLtrLbl = new System.Windows.Forms.Label();
-            this.creditHrsLbl = new System.Windows.Forms.Label();
-            this.lastNameLtrTxt = new System.Windows.Forms.TextBox();
-            this.creditHrsTxt = new System.Windows.Forms.TextBox();
-            this.registerBtn = new System.Windows.Forms.Button();
-            this.dateTimeOutputLbl = new System.Windows.Forms.Label();
-            this.extraInstructionLbl = new System.Windows.Forms.Label();
+            this.wallLengthLbl = new System.Windows.Forms.Label();
+            this.wallHeightLbl = new System.Windows.Forms.Label();
+            this.doorNumLbl = new System.Windows.Forms.Label();
+            this.numWindowsLbl = new System.Windows.Forms.Label();
+            this.numCoatsLbl = new System.Windows.Forms.Label();
+            this.wallLengthTxt = new System.Windows.Forms.TextBox();
+            this.wallHeightTxt = new System.Windows.Forms.TextBox();
+            this.numDoorsTxt = new System.Windows.Forms.TextBox();
+            this.numWindowsTxt = new System.Windows.Forms.TextBox();
+            this.numCoatsTxt = new System.Windows.Forms.TextBox();
+            this.calcBtn = new System.Windows.Forms.Button();
+            this.minGallonsLbl = new System.Windows.Forms.Label();
+            this.minGallonsOutputLbl = new System.Windows.Forms.Label();
+            this.gallonsToBuyLbl = new System.Windows.Forms.Label();
+            this.gallonsToBuyOutputLbl = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // lastNameLtrLbl
+            // wallLengthLbl
             // 
-            this.lastNameLtrLbl.AutoSize = true;
-            this.lastNameLtrLbl.Location = new System.Drawing.Point(12, 25);
-            this.lastNameLtrLbl.Name = "lastNameLtrLbl";
-            this.lastNameLtrLbl.Size = new System.Drawing.Size(246, 17);
-            this.lastNameLtrLbl.TabIndex = 0;
-            this.lastNameLtrLbl.Text = "Enter the first letter of your last name:";
+            this.wallLengthLbl.AutoSize = true;
+            this.wallLengthLbl.Location = new System.Drawing.Point(12, 48);
+            this.wallLengthLbl.Name = "wallLengthLbl";
+            this.wallLengthLbl.Size = new System.Drawing.Size(195, 17);
+            this.wallLengthLbl.TabIndex = 0;
+            this.wallLengthLbl.Text = "Total Length of walls (in feet):";
             // 
-            // creditHrsLbl
+            // wallHeightLbl
             // 
-            this.creditHrsLbl.AutoSize = true;
-            this.creditHrsLbl.Location = new System.Drawing.Point(38, 120);
-            this.creditHrsLbl.Name = "creditHrsLbl";
-            this.creditHrsLbl.Size = new System.Drawing.Size(194, 17);
-            this.creditHrsLbl.TabIndex = 1;
-            this.creditHrsLbl.Text = "Enter credit hours completed:";
+            this.wallHeightLbl.AutoSize = true;
+            this.wallHeightLbl.Location = new System.Drawing.Point(12, 96);
+            this.wallHeightLbl.Name = "wallHeightLbl";
+            this.wallHeightLbl.Size = new System.Drawing.Size(180, 17);
+            this.wallHeightLbl.TabIndex = 1;
+            this.wallHeightLbl.Text = "Height of the walls (in feet):";
             // 
-            // lastNameLtrTxt
+            // doorNumLbl
             // 
-            this.lastNameLtrTxt.Location = new System.Drawing.Point(85, 85);
-            this.lastNameLtrTxt.Name = "lastNameLtrTxt";
-            this.lastNameLtrTxt.Size = new System.Drawing.Size(100, 22);
-            this.lastNameLtrTxt.TabIndex = 2;
+            this.doorNumLbl.AutoSize = true;
+            this.doorNumLbl.Location = new System.Drawing.Point(12, 144);
+            this.doorNumLbl.Name = "doorNumLbl";
+            this.doorNumLbl.Size = new System.Drawing.Size(256, 17);
+            this.doorNumLbl.TabIndex = 2;
+            this.doorNumLbl.Text = "Number of door (non-negative integer):";
             // 
-            // creditHrsTxt
+            // numWindowsLbl
             // 
-            this.creditHrsTxt.Location = new System.Drawing.Point(85, 169);
-            this.creditHrsTxt.Name = "creditHrsTxt";
-            this.creditHrsTxt.Size = new System.Drawing.Size(100, 22);
-            this.creditHrsTxt.TabIndex = 3;
+            this.numWindowsLbl.AutoSize = true;
+            this.numWindowsLbl.Location = new System.Drawing.Point(12, 192);
+            this.numWindowsLbl.Name = "numWindowsLbl";
+            this.numWindowsLbl.Size = new System.Drawing.Size(279, 17);
+            this.numWindowsLbl.TabIndex = 3;
+            this.numWindowsLbl.Text = "Number of windows (non-negative integer):";
             // 
-            // registerBtn
+            // numCoatsLbl
             // 
-            this.registerBtn.Location = new System.Drawing.Point(63, 222);
-            this.registerBtn.Name = "registerBtn";
-            this.registerBtn.Size = new System.Drawing.Size(145, 23);
-            this.registerBtn.TabIndex = 4;
-            this.registerBtn.Text = "When do I register?";
-            this.registerBtn.UseVisualStyleBackColor = true;
-            this.registerBtn.Click += new System.EventHandler(this.RegisterBtn_Click);
+            this.numCoatsLbl.AutoSize = true;
+            this.numCoatsLbl.Location = new System.Drawing.Point(12, 240);
+            this.numCoatsLbl.Name = "numCoatsLbl";
+            this.numCoatsLbl.Size = new System.Drawing.Size(245, 17);
+            this.numCoatsLbl.TabIndex = 4;
+            this.numCoatsLbl.Text = "Coats of Paint (non-negative integer):";
             // 
-            // dateTimeOutputLbl
+            // wallLengthTxt
             // 
-            this.dateTimeOutputLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.dateTimeOutputLbl.Location = new System.Drawing.Point(25, 281);
-            this.dateTimeOutputLbl.Name = "dateTimeOutputLbl";
-            this.dateTimeOutputLbl.Size = new System.Drawing.Size(221, 23);
-            this.dateTimeOutputLbl.TabIndex = 5;
+            this.wallLengthTxt.Location = new System.Drawing.Point(331, 45);
+            this.wallLengthTxt.Name = "wallLengthTxt";
+            this.wallLengthTxt.Size = new System.Drawing.Size(100, 22);
+            this.wallLengthTxt.TabIndex = 5;
             // 
-            // extraInstructionLbl
+            // wallHeightTxt
             // 
-            this.extraInstructionLbl.AutoSize = true;
-            this.extraInstructionLbl.Location = new System.Drawing.Point(70, 53);
-            this.extraInstructionLbl.Name = "extraInstructionLbl";
-            this.extraInstructionLbl.Size = new System.Drawing.Size(126, 17);
-            this.extraInstructionLbl.TabIndex = 6;
-            this.extraInstructionLbl.Text = "(Upper Case Only)";
+            this.wallHeightTxt.Location = new System.Drawing.Point(331, 92);
+            this.wallHeightTxt.Name = "wallHeightTxt";
+            this.wallHeightTxt.Size = new System.Drawing.Size(100, 22);
+            this.wallHeightTxt.TabIndex = 6;
             // 
-            // Program2
+            // numDoorsTxt
             // 
-            this.AcceptButton = this.registerBtn;
+            this.numDoorsTxt.Location = new System.Drawing.Point(331, 139);
+            this.numDoorsTxt.Name = "numDoorsTxt";
+            this.numDoorsTxt.Size = new System.Drawing.Size(100, 22);
+            this.numDoorsTxt.TabIndex = 7;
+            // 
+            // numWindowsTxt
+            // 
+            this.numWindowsTxt.Location = new System.Drawing.Point(331, 186);
+            this.numWindowsTxt.Name = "numWindowsTxt";
+            this.numWindowsTxt.Size = new System.Drawing.Size(100, 22);
+            this.numWindowsTxt.TabIndex = 8;
+            // 
+            // numCoatsTxt
+            // 
+            this.numCoatsTxt.Location = new System.Drawing.Point(331, 233);
+            this.numCoatsTxt.Name = "numCoatsTxt";
+            this.numCoatsTxt.Size = new System.Drawing.Size(100, 22);
+            this.numCoatsTxt.TabIndex = 9;
+            // 
+            // calcBtn
+            // 
+            this.calcBtn.Location = new System.Drawing.Point(120, 323);
+            this.calcBtn.Name = "calcBtn";
+            this.calcBtn.Size = new System.Drawing.Size(136, 26);
+            this.calcBtn.TabIndex = 10;
+            this.calcBtn.Text = "Calculate";
+            this.calcBtn.UseVisualStyleBackColor = true;
+            this.calcBtn.Click += new System.EventHandler(this.CalcBtn_Click);
+            // 
+            // minGallonsLbl
+            // 
+            this.minGallonsLbl.AutoSize = true;
+            this.minGallonsLbl.Location = new System.Drawing.Point(539, 48);
+            this.minGallonsLbl.Name = "minGallonsLbl";
+            this.minGallonsLbl.Size = new System.Drawing.Size(168, 17);
+            this.minGallonsLbl.TabIndex = 11;
+            this.minGallonsLbl.Text = "Minimum gallons needed:";
+            // 
+            // minGallonsOutputLbl
+            // 
+            this.minGallonsOutputLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.minGallonsOutputLbl.Location = new System.Drawing.Point(574, 90);
+            this.minGallonsOutputLbl.Name = "minGallonsOutputLbl";
+            this.minGallonsOutputLbl.Size = new System.Drawing.Size(100, 23);
+            this.minGallonsOutputLbl.TabIndex = 12;
+            // 
+            // gallonsToBuyLbl
+            // 
+            this.gallonsToBuyLbl.AutoSize = true;
+            this.gallonsToBuyLbl.Location = new System.Drawing.Point(529, 186);
+            this.gallonsToBuyLbl.Name = "gallonsToBuyLbl";
+            this.gallonsToBuyLbl.Size = new System.Drawing.Size(207, 17);
+            this.gallonsToBuyLbl.TabIndex = 13;
+            this.gallonsToBuyLbl.Text = "Number of gallons to Purchase:";
+            // 
+            // gallonsToBuyOutputLbl
+            // 
+            this.gallonsToBuyOutputLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.gallonsToBuyOutputLbl.Location = new System.Drawing.Point(574, 233);
+            this.gallonsToBuyOutputLbl.Name = "gallonsToBuyOutputLbl";
+            this.gallonsToBuyOutputLbl.Size = new System.Drawing.Size(100, 23);
+            this.gallonsToBuyOutputLbl.TabIndex = 14;
+            // 
+            // Program1
+            // 
+            this.AcceptButton = this.calcBtn;
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(272, 355);
-            this.Controls.Add(this.extraInstructionLbl);
-            this.Controls.Add(this.dateTimeOutputLbl);
-            this.Controls.Add(this.registerBtn);
-            this.Controls.Add(this.creditHrsTxt);
-            this.Controls.Add(this.lastNameLtrTxt);
-            this.Controls.Add(this.creditHrsLbl);
-            this.Controls.Add(this.lastNameLtrLbl);
-            this.Name = "Program2";
-            this.Text = "2019 Registration";
+            this.BackColor = System.Drawing.Color.PaleGreen;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gallonsToBuyOutputLbl);
+            this.Controls.Add(this.gallonsToBuyLbl);
+            this.Controls.Add(this.minGallonsOutputLbl);
+            this.Controls.Add(this.minGallonsLbl);
+            this.Controls.Add(this.calcBtn);
+            this.Controls.Add(this.numCoatsTxt);
+            this.Controls.Add(this.numWindowsTxt);
+            this.Controls.Add(this.numDoorsTxt);
+            this.Controls.Add(this.wallHeightTxt);
+            this.Controls.Add(this.wallLengthTxt);
+            this.Controls.Add(this.numCoatsLbl);
+            this.Controls.Add(this.numWindowsLbl);
+            this.Controls.Add(this.doorNumLbl);
+            this.Controls.Add(this.wallHeightLbl);
+            this.Controls.Add(this.wallLengthLbl);
+            this.Name = "Program1";
+            this.Text = "Handy-Dandy Paint Estimator";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -118,13 +200,21 @@
 
         #endregion
 
-        private System.Windows.Forms.Label lastNameLtrLbl;
-        private System.Windows.Forms.Label creditHrsLbl;
-        private System.Windows.Forms.TextBox lastNameLtrTxt;
-        private System.Windows.Forms.TextBox creditHrsTxt;
-        private System.Windows.Forms.Button registerBtn;
-        private System.Windows.Forms.Label dateTimeOutputLbl;
-        private System.Windows.Forms.Label extraInstructionLbl;
+        private System.Windows.Forms.Label wallLengthLbl;
+        private System.Windows.Forms.Label wallHeightLbl;
+        private System.Windows.Forms.Label doorNumLbl;
+        private System.Windows.Forms.Label numWindowsLbl;
+        private System.Windows.Forms.Label numCoatsLbl;
+        private System.Windows.Forms.TextBox wallLengthTxt;
+        private System.Windows.Forms.TextBox wallHeightTxt;
+        private System.Windows.Forms.TextBox numDoorsTxt;
+        private System.Windows.Forms.TextBox numWindowsTxt;
+        private System.Windows.Forms.TextBox numCoatsTxt;
+        private System.Windows.Forms.Button calcBtn;
+        private System.Windows.Forms.Label minGallonsLbl;
+        private System.Windows.Forms.Label minGallonsOutputLbl;
+        private System.Windows.Forms.Label gallonsToBuyLbl;
+        private System.Windows.Forms.Label gallonsToBuyOutputLbl;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,8 +1,8 @@
 ﻿//Grading ID: L5899
-//Program 2
 //Section: 75
-//Due October 16, 2019
-//This program will take a user's inputs of credit hours earned and last name initial and output the date they register for classes
+//Program 1
+//Due: September 24, 2019
+//This GUI program will take users inputs of room measurements and calculate the minimum amount of paint needed to cover the room and how many gallons to purchase.
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,212 +13,46 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Program_2
+namespace Program1
 {
-    public partial class Program2 : Form
+    public partial class Program1 : Form
     {
-        public Program2()
+        public Program1()
         {
             InitializeComponent();
         }
 
-        private void RegisterBtn_Click(object sender, EventArgs e)  //Click event button that starts the program
+        //This event will take the users inputs and store them as the below listed variables and then run calculations and output the results
+        private void CalcBtn_Click(object sender, EventArgs e)
         {
-            const double senior = 90; //Senior credit hourse designation
-            const double junior = 60; //Junior credit hours designation
-            const double sophmore = 30; //Sophmore credit hours designation
-            char lastNameLtr; //User input variable for last name initial
-            double creditHrs; //User input variable for credit hours
-            string date; //Variable to store registration date for futre output
-            string time; //variable to store registration time for future output 
-            //Below Variables are upperclassmen registration time slots
-            const string upperClassAD = "11:30 am";
-            const string upperClassEI = "2:00 pm";
-            const string upperClassJO = "4:00 pm";
-            const string upperClassPS = "8:30 am";
-            const string upperClassTZ = "10:00 am";
-            //Below Variables are stored registration dates based on credit hourse for upperclassmen and credit hours and last name for underclassmen
-            const string seniorDay = "Monday, Nov. 4th";
-            const string juniorDay = "Tuesday, Nov. 5th";
-            const string firstSophDay = "Wednesday, Nov. 6th";
-            const string secondSophDay = "Thursday, Nov. 7th";
-            const string firstFreshDay = "Friday, Nov. 8th";
-            const string secondFreshDay = "Monday, Nov. 11th";
-            //Below variables are times related to underclassmen registration based on last name initial
-            const string underClassAB = "4:00 pm";
-            const string underClassCD = "8:30 am";
-            const string underClassEF = "10:00 am";
-            const string underClassGI = "11:30 am";
-            const string underClassJL = "2:00 pm";
-            const string underClassMO = "4:00 pm";
-            const string underClassPQ = "8:30 am";
-            const string underClassRS = "10:00 am";
-            const string underClassTV = "11:30 am";
-            const string underClassWZ = "2:00 pm";
-            
+            //Declaring all input, output and calculation variables
+            const int doorSubtractor = 21;
+            const int windowSubtractor = 12;
+            const int coverage = 400;
+            double lengthOfWalls;
+            double heightOfWalls;
+            int doorNum;
+            int windowNum;
+            int coatNum;
+            double squareFt;
+            double totalMinPaint;
+            int gallonsToBuy;
 
+            //Parsing inputs from the GUI form for further calculations
+            lengthOfWalls = double.Parse(wallLengthTxt.Text);
+            heightOfWalls = double.Parse(wallHeightTxt.Text);
+            doorNum = int.Parse(numDoorsTxt.Text);
+            windowNum = int.Parse(numWindowsTxt.Text);
+            coatNum = int.Parse(numCoatsTxt.Text);
 
-            if (char.TryParse(lastNameLtrTxt.Text, out lastNameLtr) && lastNameLtr >= 'A' && lastNameLtr <= 'Z') //Parsing and testing users initial input to make sure it is valid for further testing and decision making
-            {
-                if (double.TryParse(creditHrsTxt.Text, out creditHrs) && creditHrs >= 0) //Parsing and testing user credit hour input to make sure it is non-negative
-                {
-                    if (creditHrs >= senior) //Determining class based on credit hours
-                    {
-                        date = seniorDay;  //Setting registration date to Senior day
-                       //Below if/else statements will determine the time to pair with the date based on the first initial of the users last name
-                        if (lastNameLtr <= 'D')
-                        {
-                            time = upperClassAD;
-                        }
-                        else if (lastNameLtr <= 'I')
-                        {
-                            time = upperClassEI;
-                        }
-                        else if (lastNameLtr <= 'O')
-                        {
-                            time = upperClassJO;
-                        }
-                        else if (lastNameLtr <= 'S')
-                        {
-                            time = upperClassPS;
-                        }
-                        else
-                        {
-                            time = upperClassTZ;
-                        }
-                    }
-                    else if (creditHrs >= junior) //Determining class based on credit hours
-                    {
-                        date = juniorDay; //Setting date for junior registeration
-                        //Below if/else statements will determine the time to pair with the date based on the first initial of the users last name
-                        if (lastNameLtr <= 'D')
-                        {
-                            time = upperClassAD;
-                        }
-                        else if (lastNameLtr <= 'I')
-                        {
-                            time = upperClassEI;
-                        }
-                        else if (lastNameLtr <= 'O')
-                        {
-                            time = upperClassJO;
-                        }
-                        else if (lastNameLtr <= 'S')
-                        {
-                            time = upperClassPS;
-                        }
-                        else
-                        {
-                            time = upperClassTZ;
-                        }
-                    }
-                    else if (creditHrs >= sophmore && (lastNameLtr <= 'B' || lastNameLtr >= 'P')) //Determining which day underclassmen will register based on credit hours and name initial input
-                    {
-                        date = firstSophDay; //Setting date variable to sophmore day 1
-                        //Below if/else statements will determine the time to pair with the date based on the first initial of the users last name
-                        if (lastNameLtr >= 'W')
-                        {
-                            time = underClassWZ;
-                        }
-                        else if (lastNameLtr >= 'T')
-                        {
-                            time = underClassTV;
-                        }
-                        else if (lastNameLtr >= 'R')
-                        {
-                            time = underClassRS;
-                        }
-                        else if (lastNameLtr >= 'P')
-                        {
-                            time = underClassPQ;
-                        }
-                        else
-                        {
-                            time = underClassAB;
-                        }
-                    }
-                    else if (creditHrs >= sophmore && lastNameLtr >= 'C' && lastNameLtr <= 'O') //Determining which day underclassmen will register based on credit hours and last name's first initial
-                    {
-                        date = secondSophDay; //Setting date to second sophmore day
-                        //Below if/else statements will determine the time to pair with the date based on the first initial of the users last name
-                        if (lastNameLtr <= 'D')
-                        {
-                            time = underClassCD;
-                        }
-                        else if (lastNameLtr <= 'F')
-                        {
-                            time = underClassEF;
-                        }
-                        else if (lastNameLtr <= 'I')
-                        {
-                            time = underClassGI;
-                        }
-                        else if (lastNameLtr <= 'L')
-                        {
-                            time = underClassJL;
-                        }
-                        else
-                        {
-                            time = underClassMO;
-                        }
-                    }
-                    else if (lastNameLtr <= 'B' || lastNameLtr >= 'P')  //Will determine which of the two days freshmen will register
-                    {
-                        date = firstFreshDay;  //Date will be set based on credit hours and last name first initial for freshmen and sophmores
-                        //Below if/else statements will determine the time to pair with the date based on the first initial of the users last name
-                        if (lastNameLtr >= 'W')
-                        {
-                            time = underClassWZ;
-                        }
-                        else if (lastNameLtr >= 'T')
-                        {
-                            time = underClassTV;
-                        }
-                        else if (lastNameLtr >= 'R')
-                        {
-                            time = underClassRS;
-                        }
-                        else if (lastNameLtr >= 'P')
-                        {
-                            time = underClassPQ;
-                        }
-                        else
-                        {
-                            time = underClassAB;
-                        }
-                    }
-                    else
-                    {
-                        date = secondFreshDay;  //setting date to the last day for freshmen to register
-                        //Below if/else statements will determine the time to pair with the date based on the first initial of the users last name
-                        if (lastNameLtr <= 'D')
-                        {
-                            time = underClassCD;
-                        }
-                        else if (lastNameLtr <= 'F')
-                        {
-                            time = underClassEF;
-                        }
-                        else if (lastNameLtr <= 'I')
-                        {
-                            time = underClassGI;
-                        }
-                        else if (lastNameLtr <= 'L')
-                        {
-                            time = underClassJL;
-                        }
-                        else
-                        {
-                            time = underClassMO;
-                        }
-                    }
-                    dateTimeOutputLbl.Text = $"{date} @ {time}";  //Output to GUI text box showing the date and time for registration
+            //Calculating the minimum paint needed and the number of gallons to puchase
+            squareFt = (lengthOfWalls * heightOfWalls) - (doorNum * doorSubtractor) - (windowNum * windowSubtractor);
+            totalMinPaint = (squareFt * coatNum) / coverage;
+            gallonsToBuy = (int)Math.Ceiling(totalMinPaint);
 
-                }
-                else MessageBox.Show("Enter a valid number of credit hours"); //Error message to display for invalid number of credit hours
-            }
-                else MessageBox.Show("Enter a valid last initial");  //Error message to display for invalid last name initial
+            //Output to the GUI form post calulations
+            minGallonsOutputLbl.Text = $"{totalMinPaint:F1}";
+            gallonsToBuyOutputLbl.Text = $"{gallonsToBuy}";
         }
-
     }
 }
