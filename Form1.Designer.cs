2c54b5a4d4bce17de3bbf9e01535034a41c30aa8
Alex Edelen <55199498+edelenalex@users.noreply.github.com>
2020-12-01 21:21:40 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Program_2
+{
+    partial class Program2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lastNameLtrLbl = new System.Windows.Forms.Label();
+            this.creditHrsLbl = new System.Windows.Forms.Label();
+            this.lastNameLtrTxt = new System.Windows.Forms.TextBox();
+            this.creditHrsTxt = new System.Windows.Forms.TextBox();
+            this.registerBtn = new System.Windows.Forms.Button();
+            this.dateTimeOutputLbl = new System.Windows.Forms.Label();
+            this.extraInstructionLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lastNameLtrLbl
+            // 
+            this.lastNameLtrLbl.AutoSize = true;
+            this.lastNameLtrLbl.Location = new System.Drawing.Point(12, 25);
+            this.lastNameLtrLbl.Name = "lastNameLtrLbl";
+            this.lastNameLtrLbl.Size = new System.Drawing.Size(246, 17);
+            this.lastNameLtrLbl.TabIndex = 0;
+            this.lastNameLtrLbl.Text = "Enter the first letter of your last name:";
+            // 
+            // creditHrsLbl
+            // 
+            this.creditHrsLbl.AutoSize = true;
+            this.creditHrsLbl.Location = new System.Drawing.Point(38, 120);
+            this.creditHrsLbl.Name = "creditHrsLbl";
+            this.creditHrsLbl.Size = new System.Drawing.Size(194, 17);
+            this.creditHrsLbl.TabIndex = 1;
+            this.creditHrsLbl.Text = "Enter credit hours completed:";
+            // 
+            // lastNameLtrTxt
+            // 
+            this.lastNameLtrTxt.Location = new System.Drawing.Point(85, 85);
+            this.lastNameLtrTxt.Name = "lastNameLtrTxt";
+            this.lastNameLtrTxt.Size = new System.Drawing.Size(100, 22);
+            this.lastNameLtrTxt.TabIndex = 2;
+            // 
+            // creditHrsTxt
+            // 
+            this.creditHrsTxt.Location = new System.Drawing.Point(85, 169);
+            this.creditHrsTxt.Name = "creditHrsTxt";
+            this.creditHrsTxt.Size = new System.Drawing.Size(100, 22);
+            this.creditHrsTxt.TabIndex = 3;
+            // 
+            // registerBtn
+            // 
+            this.registerBtn.Location = new System.Drawing.Point(63, 222);
+            this.registerBtn.Name = "registerBtn";
+            this.registerBtn.Size = new System.Drawing.Size(145, 23);
+            this.registerBtn.TabIndex = 4;
+            this.registerBtn.Text = "When do I register?";
+            this.registerBtn.UseVisualStyleBackColor = true;
+            this.registerBtn.Click += new System.EventHandler(this.RegisterBtn_Click);
+            // 
+            // dateTimeOutputLbl
+            // 
+            this.dateTimeOutputLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.dateTimeOutputLbl.Location = new System.Drawing.Point(25, 281);
+            this.dateTimeOutputLbl.Name = "dateTimeOutputLbl";
+            this.dateTimeOutputLbl.Size = new System.Drawing.Size(221, 23);
+            this.dateTimeOutputLbl.TabIndex = 5;
+            // 
+            // extraInstructionLbl
+            // 
+            this.extraInstructionLbl.AutoSize = true;
+            this.extraInstructionLbl.Location = new System.Drawing.Point(70, 53);
+            this.extraInstructionLbl.Name = "extraInstructionLbl";
+            this.extraInstructionLbl.Size = new System.Drawing.Size(126, 17);
+            this.extraInstructionLbl.TabIndex = 6;
+            this.extraInstructionLbl.Text = "(Upper Case Only)";
+            // 
+            // Program2
+            // 
+            this.AcceptButton = this.registerBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(272, 355);
+            this.Controls.Add(this.extraInstructionLbl);
+            this.Controls.Add(this.dateTimeOutputLbl);
+            this.Controls.Add(this.registerBtn);
+            this.Controls.Add(this.creditHrsTxt);
+            this.Controls.Add(this.lastNameLtrTxt);
+            this.Controls.Add(this.creditHrsLbl);
+            this.Controls.Add(this.lastNameLtrLbl);
+            this.Name = "Program2";
+            this.Text = "2019 Registration";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lastNameLtrLbl;
+        private System.Windows.Forms.Label creditHrsLbl;
+        private System.Windows.Forms.TextBox lastNameLtrTxt;
+        private System.Windows.Forms.TextBox creditHrsTxt;
+        private System.Windows.Forms.Button registerBtn;
+        private System.Windows.Forms.Label dateTimeOutputLbl;
+        private System.Windows.Forms.Label extraInstructionLbl;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,224 @@
+﻿//Grading ID: L5899
+//Program 2
+//Section: 75
+//Due October 16, 2019
+//This program will take a user's inputs of credit hours earned and last name initial and output the date they register for classes
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program_2
+{
+    public partial class Program2 : Form
+    {
+        public Program2()
+        {
+            InitializeComponent();
+        }
+
+        private void RegisterBtn_Click(object sender, EventArgs e)  //Click event button that starts the program
+        {
+            const double senior = 90; //Senior credit hourse designation
+            const double junior = 60; //Junior credit hours designation
+            const double sophmore = 30; //Sophmore credit hours designation
+            char lastNameLtr; //User input variable for last name initial
+            double creditHrs; //User input variable for credit hours
+            string date; //Variable to store registration date for futre output
+            string time; //variable to store registration time for future output 
+            //Below Variables are upperclassmen registration time slots
+            const string upperClassAD = "11:30 am";
+            const string upperClassEI = "2:00 pm";
+            const string upperClassJO = "4:00 pm";
+            const string upperClassPS = "8:30 am";
+            const string upperClassTZ = "10:00 am";
+            //Below Variables are stored registration dates based on credit hourse for upperclassmen and credit hours and last name for underclassmen
+            const string seniorDay = "Monday, Nov. 4th";
+            const string juniorDay = "Tuesday, Nov. 5th";
+            const string firstSophDay = "Wednesday, Nov. 6th";
+            const string secondSophDay = "Thursday, Nov. 7th";
+            const string firstFreshDay = "Friday, Nov. 8th";
+            const string secondFreshDay = "Monday, Nov. 11th";
+            //Below variables are times related to underclassmen registration based on last name initial
+            const string underClassAB = "4:00 pm";
+            const string underClassCD = "8:30 am";
+            const string underClassEF = "10:00 am";
+            const string underClassGI = "11:30 am";
+            const string underClassJL = "2:00 pm";
+            const string underClassMO = "4:00 pm";
+            const string underClassPQ = "8:30 am";
+            const string underClassRS = "10:00 am";
+            const string underClassTV = "11:30 am";
+            const string underClassWZ = "2:00 pm";
+            
+
+
+            if (char.TryParse(lastNameLtrTxt.Text, out lastNameLtr) && lastNameLtr >= 'A' && lastNameLtr <= 'Z') //Parsing and testing users initial input to make sure it is valid for further testing and decision making
+            {
+                if (double.TryParse(creditHrsTxt.Text, out creditHrs) && creditHrs >= 0) //Parsing and testing user credit hour input to make sure it is non-negative
+                {
+                    if (creditHrs >= senior) //Determining class based on credit hours
+                    {
+                        date = seniorDay;  //Setting registration date to Senior day
+                       //Below if/else statements will determine the time to pair with the date based on the first initial of the users last name
+                        if (lastNameLtr <= 'D')
+                        {
+                            time = upperClassAD;
+                        }
+                        else if (lastNameLtr <= 'I')
+                        {
+                            time = upperClassEI;
+                        }
+                        else if (lastNameLtr <= 'O')
+                        {
+                            time = upperClassJO;
+                        }
+                        else if (lastNameLtr <= 'S')
+                        {
+                            time = upperClassPS;
+                        }
+                        else
+                        {
+                            time = upperClassTZ;
+                        }
+                    }
+                    else if (creditHrs >= junior) //Determining class based on credit hours
+                    {
+                        date = juniorDay; //Setting date for junior registeration
+                        //Below if/else statements will determine the time to pair with the date based on the first initial of the users last name
+                        if (lastNameLtr <= 'D')
+                        {
+                            time = upperClassAD;
+                        }
+                        else if (lastNameLtr <= 'I')
+                        {
+                            time = upperClassEI;
+                        }
+                        else if (lastNameLtr <= 'O')
+                        {
+                            time = upperClassJO;
+                        }
+                        else if (lastNameLtr <= 'S')
+                        {
+                            time = upperClassPS;
+                        }
+                        else
+                        {
+                            time = upperClassTZ;
+                        }
+                    }
+                    else if (creditHrs >= sophmore && (lastNameLtr <= 'B' || lastNameLtr >= 'P')) //Determining which day underclassmen will register based on credit hours and name initial input
+                    {
+                        date = firstSophDay; //Setting date variable to sophmore day 1
+                        //Below if/else statements will determine the time to pair with the date based on the first initial of the users last name
+                        if (lastNameLtr >= 'W')
+                        {
+                            time = underClassWZ;
+                        }
+                        else if (lastNameLtr >= 'T')
+                        {
+                            time = underClassTV;
+                        }
+                        else if (lastNameLtr >= 'R')
+                        {
+                            time = underClassRS;
+                        }
+                        else if (lastNameLtr >= 'P')
+                        {
+                            time = underClassPQ;
+                        }
+                        else
+                        {
+                            time = underClassAB;
+                        }
+                    }
+                    else if (creditHrs >= sophmore && lastNameLtr >= 'C' && lastNameLtr <= 'O') //Determining which day underclassmen will register based on credit hours and last name's first initial
+                    {
+                        date = secondSophDay; //Setting date to second sophmore day
+                        //Below if/else statements will determine the time to pair with the date based on the first initial of the users last name
+                        if (lastNameLtr <= 'D')
+                        {
+                            time = underClassCD;
+                        }
+                        else if (lastNameLtr <= 'F')
+                        {
+                            time = underClassEF;
+                        }
+                        else if (lastNameLtr <= 'I')
+                        {
+                            time = underClassGI;
+                        }
+                        else if (lastNameLtr <= 'L')
+                        {
+                            time = underClassJL;
+                        }
+                        else
+                        {
+                            time = underClassMO;
+                        }
+                    }
+                    else if (lastNameLtr <= 'B' || lastNameLtr >= 'P')  //Will determine which of the two days freshmen will register
+                    {
+                        date = firstFreshDay;  //Date will be set based on credit hours and last name first initial for freshmen and sophmores
+                        //Below if/else statements will determine the time to pair with the date based on the first initial of the users last name
+                        if (lastNameLtr >= 'W')
+                        {
+                            time = underClassWZ;
+                        }
+                        else if (lastNameLtr >= 'T')
+                        {
+                            time = underClassTV;
+                        }
+                        else if (lastNameLtr >= 'R')
+                        {
+                            time = underClassRS;
+                        }
+                        else if (lastNameLtr >= 'P')
+                        {
+                            time = underClassPQ;
+                        }
+                        else
+                        {
+                            time = underClassAB;
+                        }
+                    }
+                    else
+                    {
+                        date = secondFreshDay;  //setting date to the last day for freshmen to register
+                        //Below if/else statements will determine the time to pair with the date based on the first initial of the users last name
+                        if (lastNameLtr <= 'D')
+                        {
+                            time = underClassCD;
+                        }
+                        else if (lastNameLtr <= 'F')
+                        {
+                            time = underClassEF;
+                        }
+                        else if (lastNameLtr <= 'I')
+                        {
+                            time = underClassGI;
+                        }
+                        else if (lastNameLtr <= 'L')
+                        {
+                            time = underClassJL;
+                        }
+                        else
+                        {
+                            time = underClassMO;
+                        }
+                    }
+                    dateTimeOutputLbl.Text = $"{date} @ {time}";  //Output to GUI text box showing the date and time for registration
+
+                }
+                else MessageBox.Show("Enter a valid number of credit hours"); //Error message to display for invalid number of credit hours
+            }
+                else MessageBox.Show("Enter a valid last initial");  //Error message to display for invalid last name initial
+        }
+
+    }
+}
